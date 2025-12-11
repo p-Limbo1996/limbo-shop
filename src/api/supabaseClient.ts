b37a4b69a983ced2bbc1a638dc5984efd343437a
LimboCode <121183821+p-Limbo1996@users.noreply.github.com>
2025-12-11 17:03:59 +0330

    Remove supabaseClient.ts from repository

--- src/api/supabaseClient.ts
+++
@@ -1,8 +0,0 @@
-import { createClient, SupabaseClient } from "@supabase/supabase-js";
-
-const APP_SUPABASE_URL:string = "https://hadlmawwdetqyspyjsaf.supabase.co";
-const APP_SUPABASE_KEY:string ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhhZGxtYXd3ZGV0cXlzcHlqc2FmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2NjMxNzAsImV4cCI6MjA4MDIzOTE3MH0.A3MLnK-2vMdq9xZF0ST-PiBGATSjTVHohCQtUeaESt4";
-
-const supabase:SupabaseClient = createClient(APP_SUPABASE_URL, APP_SUPABASE_KEY);
-
-export default supabase;
